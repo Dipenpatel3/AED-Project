f632b5498d0aba98a136ee188a84ba4815d51081
Dipen Manoj Patel <patel.dipe@northeastern.edu>
2023-10-20 20:06:50 -0400

    Initial start of project

---
+++ Assignment project/src/MainModel/StudentClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainModel;
+
+/**
+ *
+ * @author dipenpatel
+ */
+public class StudentClass {
+    private String FirstName;
+    private String LastName;
+    private String ContactDetails;
+    private String UserID;
+    private String Password;
+    private String NEUID;
+    private String EmailId;
+           
+}
